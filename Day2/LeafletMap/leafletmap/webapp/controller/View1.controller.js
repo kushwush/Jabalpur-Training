fa3d795484399845e15627dbd21e9877012eb208
Khushal Sharma <khus03442@gmail.com>
2024-05-19 02:32:47 +0530

    Add Leaflet map to project

--- Day2/LeafletMap/leafletmap/webapp/controller/View1.controller.js
+++ Day2/LeafletMap/leafletmap/webapp/controller/View1.controller.js
@@ -10,6 +10,18 @@ sap.ui.define([
         return Controller.extend("leafletmap.controller.View1", {
             onInit: function () {
 
+            },
+
+            onAfterRendering: function () {
+                let mapPanel = this.getView().byId("map");
+                var map = L.map(mapPanel.getId()).setView([51.505, -0.09], 13);
+
+                L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_background/{z}/{x}/{y}{r}.{ext}', {
+                    minZoom: 0,
+                    maxZoom: 20,
+                    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                    ext: 'png'
+                }).addTo(map);
             }
         });
     });
